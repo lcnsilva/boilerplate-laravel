1c9dc0e99a00716c3b3c5e370a4958b713894fc3
lcnsilva <lcnsilvajf@gmail.com>
2026-04-23 22:48:32 -0300

    feat: add repository and unauthorized acess in default route.

---
+++ app/Repositories/ItemRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Item;
+
+class ItemRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function getAll(): array
+    {
+        return Item::query()->orderByDesc('id')->get();
+    }
+
+    public function getById(int $id): ?Item
+    {
+        return Item::find($id);
+    }
+
+    public function create (array $data): Item
+    {
+        return Item::create($data);
+    }
+
+    public function update(Item $item, array $data): Item
+    {
+        $item->update($data);
+        return $item->refresh();
+    }
+
+    public function delete(Item $item): void
+    {
+        $item->delete();
+        //add deleted_at depois para implementar soft delete
+    }
+}
